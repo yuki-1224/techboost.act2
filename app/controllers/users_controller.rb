903135ff58da5d3579b4034062750b5cf658e9c2
”takeyu” <youxizhn4116@gmail.com>
2020-09-12 01:35:54 +0900

    rails04

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,16 @@ class UsersController < ApplicationController
 
   def create
     # binding.pry
-    user = User.new(name: params[:name], email: params[:email])
-    if user.save
-      redirect_to root_path
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: '登録完了'
     else
+      flash.now[:danger] = '登録失敗'
       render :new
     end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  EMAIL_REGULAR = /\A([^@\s]+)@[a-zA-Z0-9]+\.[a-zA-Z0-9-\.]+\z/
+  PASS_REGULAR = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/
+
+  validates :name, presence: true, length: {maximum: 15}
+  validates :email, presence: true, uniqueness: true, format: {with: EMAIL_REGULAR}
+  validates :password, presence: true, format: {with: PASS_REGULAR}
+
+  has_secure_password
 end
